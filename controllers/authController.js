205b18ffbb90182baed7eacb3820f6799bd05559
Kushagra Singh <73294651+KushagraSingh02@users.noreply.github.com>
2023-06-21 01:27:20 +0530

    register complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,68 @@
-export const registerController = () =>{
+import  {hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModel.js";
+
+export const registerController = async (req,res) =>{
+
+    try {
+        
+        const {name,email,password,phone,address} = req.body
+        //validaitons
+
+        if(!name){
+
+            res.send({error:'Name is required'})
+        }
+        if(!email){
+
+            res.send({error:'email is required'})
+        }
+        if(!password){
+
+            res.send({error:'password is required'})
+        }
+        if(!phone){
+
+            res.send({error:'phone is required'})
+        }
+        if(!address){
+
+            res.send({error:'address is required'})
+        }
+
+        //check user
+        const existinguser = await userModel.findOne({email});
+
+        //existing user
+        if(existinguser){
+
+            return res.status(200).send({
+                success:true,
+                message:"Already Register please login"
+            })
+        }
+
+        //register user
+        const hashedPassword = await hashPassword(password)
+
+        //save
+        const user = new userModel({name,email,phone,address,password:hashedPassword}).save()
+
+        res.status(200).send({
+            success:true,
+            message:"Registration successful",
+            user
+        })
+
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).send({
+            success:false,
+            message:'Error in registration',
+            error
+        })
+    }
 
 };
 
--- server.js
+++ server.js
@@ -6,6 +6,31 @@ import mogan from 'morgan'
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js"
 //configure env
+// import { MongoClient } from "mongodb";
+// import { ServerApiVersion } from "mongodb";
+// const uri = "mongodb+srv://kushagra:kushagra@cluster0.sui78iu.mongodb.net/?retryWrites=true&w=majority";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("ecommerce").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 dotenv.config();
 
 //connect database
